b877e191081785ac24c3bf1aacbd9119c04196d4
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-09 12:38:03 -0500

    add all EQ markers and bind pop-ups data

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -12,9 +12,9 @@ let background = {
 //https://leafletjs.com/examples/layers-control/, https://leafletjs.com/reference.html#layergroup
 let allEarthquakes = new L.LayerGroup();
 
-// initialize map with standard background
+// initialize map with standard background -- lat/long are for san andreas fault line
 let map = L.map("map-id", {
-	center: [40.7, -94.5],
+	center: [35.8, -119.4],
 	zoom: 3,
   layers: [standard]});
 
@@ -25,3 +25,44 @@ let overlay = {
 // create control layer -- add overlay and background map
 L.control.layers(background, overlay).addTo(map);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// store URL for all earthquake data
+let urlEarthquakes = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
+// define function to read json data, add earthquake markers to map
+function init() {
+    
+    // use d3 to ingest earthquake data from url
+    d3.json(urlEarthquakes).then(function(data) {
+
+        // declare and initialize objects for visualizations
+        let features = data.features;
+
+        // initialize array to see all earthquake depths
+        let depthArray = []
+
+        // for loop to iterate through each earthquake
+        for (var f = 0; f < features.length; f++) {
+            
+            // declare and initialize coordinate objects -- lat/long/depth
+            let coords = features[f].geometry.coordinates;
+            let lat = coords[1];
+            let lng = coords[0];
+            let depth = coords[2];
+            depthArray.push(depth);
+
+            // declare and initialize properties objects
+            let properties = features[f].properties;
+            let location = properties.place;
+            let size = properties.mag;
+            
+            // create marker for each earthquake. bind popup with earthquake information
+            L.marker([lat, lng]).bindPopup(`<h3>${location}</h3><br/>Magnitude: ${size}<br/>Depth: ${depth}`).addTo(allEarthquakes)};    
+    });
+
+    // add markers to map
+    allEarthquakes.addTo(map);
+};
+
+init();
