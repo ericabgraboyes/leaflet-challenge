c76b0d6b89a494787d3d2cb9605aeabbf2150e87
erica.b.graboyes <erica.b.graboyes@gmail.com>
2023-04-12 11:26:04 -0500

    simplified syntax

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -1,3 +1,4 @@
+//////////////////////////////////---  MAP SETUP - ///////////////////////////////////////////
 // initialize map object with standard background -- lat/long are for san andreas fault line
 let map = L.map("map-id", {
   center: [40.5, -90.5],
@@ -11,7 +12,7 @@ standard = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // add tile layer to map
 standard.addTo(map)
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////---  CIRCLE COLORS & DEPTH  ----- ///////////////////////////////////////////
 // functions to style earthquake markers / https://www.color-hex.com/color-palettes/?page=4 (color scheme)
 
 // define function to determine circle color based on earthquake depth
@@ -34,9 +35,11 @@ if(size === 0) {
   return 1;}
   return size * 3};
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// set up legend for circle markers
+///////////////////////////////////---    LEGEND   ---- //////////////////////////////////////////////////////
+// create a legend
 let legend = L.control({position: "bottomright"});
+
+// define legend function
 legend.onAdd = function() {
   let div = L.DomUtil.create("div", "info legend");
   const depths = ["<-10", 10, 30, 50, 70, 90];
@@ -53,69 +56,69 @@ legend.onAdd = function() {
     depths[i] + (depths[i + 1] ? " to " + depths[i + 1] + "<br>" : "+");}
   return div;};
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////--- EARTHQUAKE DATA ----////////////////////////////////////////////////////
 // store URL for all earthquake data
 let urlEarthquakes = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
 // define function to read json data, add earthquake markers to map
-function init() {
+function earthquakes() {
     
-    // use d3 to ingest earthquake data from url
-    d3.json(urlEarthquakes).then(function(data) {
+  // use d3 to ingest earthquake data from url
+  d3.json(urlEarthquakes).then(function(data) {
+          
+    // declare and initialize objects for visualizations
+    let features = data.features;
+    // console.log(data)
 
-        // declare and initialize objects for visualizations
-        let features = data.features;
-        // console.log(data)
+    // // initialize array to see all earthquake depths
+    // let depthArray = []
 
-        // // initialize array to see all earthquake depths
-        // let depthArray = []
+    // can also use .filter and arrow function to filter data; apply for loop to new variable.
+    // let filteredata = features.filter(f => f.geometry.coordinates[2] >= 500)
+    // console.log(filteredata)
 
-        // can also use .filter and arrow function to filter data; apply for loop to new variable.
-        // let filteredata = features.filter(f => f.geometry.coordinates[2] >= 500)
-        // console.log(filteredata)
-
-        // for loop to iterate through each earthquake
-        for (var f = 0; f < features.length; f++) {
+    // for loop to iterate through each earthquake
+    for (var f = 0; f < features.length; f++) {
             
-            // declare and initialize coordinate objects -- lat/long/depth
-            let coords = features[f].geometry.coordinates;
-            // console.log("testcoords:",coords)
+      // declare and initialize coordinate objects -- lat/long/depth
+      let coords = features[f].geometry.coordinates;
+        // console.log("testcoords:",coords)
 
-            // //use conditional statement to filter records for given depth; filters all records 
-            // if (coords[2] >= 10) {
+        // use conditional statement to filter records for given depth; filters all records 
+        // if (coords[2] >= 10) {
 
-            let lat = coords[1];
-            let lng = coords[0];
-            let depth = coords[2];
+          let lat = coords[1];
+          let lng = coords[0];
+          let depth = coords[2];
 
-            // declare and initialize properties objects
-            let properties = features[f].properties;
-            let location = properties.place;
-            let size = properties.mag;
+          // declare and initialize properties objects
+          let properties = features[f].properties;
+          let location = properties.place;
+          let size = properties.mag;
 
-            // convert time from milliseconds since epoch to normal date/time
-            let date = new Date (properties.time);
+          // convert time from milliseconds since epoch to normal date/time
+          let date = new Date (properties.time);
 
-            // create marker for each earthquake. bind popup with earthquake information
-            L.circleMarker([lat, lng], {
-                fillOpacity:1,
-                clickable: true,
-                stroke: true,
-                color: "black",
-                weight: 0.25,
-                fillColor: setColor(depth),
-                radius: setSize(size)})
+          // create marker for each earthquake. bind popup with earthquake information
+          L.circleMarker([lat, lng], {
+              fillOpacity:1,
+              clickable: true,
+              stroke: true,
+              color: "black",
+              weight: 0.25,
+              fillColor: setColor(depth),
+              radius: setSize(size)})
             .bindPopup(`<h4>${location}</h4><b>Magnitude</b>: ${size}<br/><b>Reported Depth (km):</b> ${depth} km<br>Time: ${date.toDateString()}`).addTo(map)
-        };
+    };
       
             // // generate list of earthquake depths for color function
             // var allDepths = depthArray.map((e, i) => (e)).join('/')
             // console.log('All depths',allDepths);            
             
-    });
+  });
     // add legend to map
-    legend.addTo(map)
+  legend.addTo(map)
     
 };
 
-init();
+earthquakes();
